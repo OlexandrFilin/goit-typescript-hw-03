bb2fab987406e4422a9d7c0257fc361e01281bfc
OlexandrFilin <acfiln@ukr.net>
2023-11-27 21:17:56 +0200

    The first version of the tasks solution

--- src/2.ts
+++ src/2.ts
@@ -1,5 +1,5 @@
 /*
-  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
+  Ваше /*завдання полягатиме у створенні двох класів – Employee та Manager.
 
   Клас Employee повинен включати:
 
@@ -14,13 +14,31 @@
 
 */
 
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
+
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  
+  constructor(public name: string, private department: string, protected salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +51,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor ( name : string,department: string,salary:number){
+    super(name,department,salary+10000)
+  }
+  
 }
-
-
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean=false;
+  tenants:Person[]=[];
+  keyHouse: Key;
+  constructor( key: Key) {
+       this.keyHouse = key;
+  }
+  comeIn(person:Person){
+    if(this.door && !this.tenants.includes(person)){
+        this.tenants.push(person);
+    } else {
+        alert('WARNING. This person is already at home');
+    }
+  }
+  abstract openDoor(key:Key):void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key) {
+        if(key.getSignature()===this.keyHouse.getSignature()){
+            this.door = true;
+            alert('The door is open');
+        } else {
+            alert('The key does not fit the lock');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
